f21ee83d78af49156be97ba27f218c7c969368f4
Eduarda2007aaa <126172030+Eduarda2007aaa@users.noreply.github.com>
2024-10-04 08:56:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
-
-    tecla.onclick = function () {
-        tocaSom(idAudio);
+const perguntas = [
+    {
+        enunciado:"Assim que saiu da escola você se depara co uma tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas.Qual o primeiro pensamento?",
+        alternativas: [
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada inteligência Artificial (IA),uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia.No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula.Qual atitude você toma?",
+        alternativas: [
+            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento",
+            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos própios sobre o tema."
+        ]
+    },
+    {
+        enunciado: "Após a elaboraçãomdo trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita.Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro.Nesse debate,como você se posiciona?",
+        alternativas:[
+            
+        ]
     }
-
-}
+            
+        
+        
+    }
+]
