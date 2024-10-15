aa03715b7aef63a747b6d29e12eb967b6a8379ae
Eduarda2007aaa <126172030+Eduarda2007aaa@users.noreply.github.com>
2024-10-15 09:40:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -22,9 +22,35 @@ const perguntas = [
     {
         enunciado: "Após a elaboraçãomdo trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita.Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro.Nesse debate,como você se posiciona?",
         alternativas:[
-            
+            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            "Me preucupo com as pessoas que perderam seus empregos para máquinas  defendem a impontância de proteger os trabalhadores.",
         ]
-    }
+    },
+    {
+        enunciado: "Ao final da discussão, você prescisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            "Criar uma imagem utilizando um gerador de imagem de IA."
+        ]
+    },
+    {
+        enunciado: "Vocẽ tem o trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA.O problema é que o trabalho está totalmente igual ao do chat.O que vocẽ faz?",
+        alternativas: [
+            "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+            "O chat pode ser uma tecnologia muito avançada, mas é presciso manter atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual= perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado]
+}
+
+mostraPergunta();
             
         
         
